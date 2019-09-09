6d54bc7d053b4a9a5ca373330206a0e600f3b728
Arvid Lundqvist <arvlun0221@skynet.halmstad.se>
2019-09-09 14:10:55 +0200

    Påbröjar, ej klar med Calculate-knappen

---
+++ Miniräknare/Miniräknare/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Miniräknare
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int oprtr = 1;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            int oprtr = 2;
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            int oprtr = 3;
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            int oprtr = 4;
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            if(oprtr = 1)
+            { textBox1 + textBox2}
+        }
+    }
+}
