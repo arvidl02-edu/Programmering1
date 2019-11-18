1cc3ecb83b700f5fd78b8155bd41631d63ac10e4
Arvid Lundqvist <arvlun0221@skynet.halmstad.se>
2019-11-18 14:18:59 +0100

    ja

--- Metoder/Övn. 10.2/Form1.Designer.cs
+++ Metoder/Övn. 10.2/Form1.Designer.cs
@@ -35,45 +35,49 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(217, 136);
+            this.button1.Location = new System.Drawing.Point(50, 86);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(57, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Beräkna";
+            this.toolTip1.SetToolTip(this.button1, "true");
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(47, 47);
+            this.label1.Location = new System.Drawing.Point(47, 39);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "Radie";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(50, 89);
+            this.label2.Location = new System.Drawing.Point(47, 65);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(29, 13);
             this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
+            this.label2.Text = "Höjd";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(217, 39);
+            this.textBox1.Location = new System.Drawing.Point(113, 36);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 3;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(217, 81);
+            this.textBox2.Location = new System.Drawing.Point(113, 62);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 4;
@@ -82,11 +86,30 @@
             // 
             this.toolTip1.Popup += new System.Windows.Forms.PopupEventHandler(this.toolTip1_Popup);
             // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(113, 88);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(219, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "längdenhet^3";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(449, 360);
+            this.ClientSize = new System.Drawing.Size(303, 144);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
@@ -107,6 +130,8 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Metoder/Övn. 10.2/Form1.cs
+++ Metoder/Övn. 10.2/Form1.cs
@@ -17,9 +17,25 @@ namespace Övn._10._2
             InitializeComponent();
         }
 
+        double radieInput = 0;
+        double volymInput = 0;
+
+        double volymCylinder(double radie, double höjd)
+        {
+            double volym = radie * radie * Math.PI * höjd;
+            return volym;
+        }
+
         private void toolTip1_Popup(object sender, PopupEventArgs e)
         {
-            MessageBox.Show("Yeeeeaaah boyyy", "HAHAHAHHAHA", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            MessageBox.Show("Yeeeeaaah boyyy", "HAHAHAHHAHA", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            radieInput = double.Parse(textBox1.Text);
+            volymInput = double.Parse(textBox2.Text);
+            textBox3.Text = volymCylinder(radieInput, volymInput)+"";
         }
     }
 }
